7512cdde3765c2c9243c156e154460ceb0b9f6db
Esmeralda <mtz.esmeralda1503@gmail.com>
2018-03-12 19:25:12 -0600

    Added a script for rfc

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,3 +33,23 @@ $(document).ready(function(){
     return false;
   }
 });
+
+function calcular(nombre,ap_pater,ap_mater, fecha){
+  if(nombre != "" & ap_pater != "" & ap_mater != "" & fecha != ""){
+    var rfc, cad_fecha, vocal,cont=0;
+    var vocales = ["a","e","i","o","u"];
+    var arreglo = ap_pater.substring(1,ap_pater.length).split('');
+    for(var i=0;i<arreglo.length;i++){
+      for(var j=0;j<vocales.length;j++){
+        if(arreglo[i]==vocales[j] & cont==0){
+          vocal = arreglo[i];
+          cont=cont+1;
+        }
+      }
+    }
+    cad_fecha = fecha.split("-", fecha.length);
+    rfc = ap_pater.substring(0,1) + vocal + ap_mater.substring(0,1) + nombre.substring(0,1)+ cad_fecha[0].substring(2,4)+cad_fecha[1]+cad_fecha[2];
+    rfc= rfc.toUpperCase();
+    document.getElementById('rfc').value = rfc;
+  }
+}
